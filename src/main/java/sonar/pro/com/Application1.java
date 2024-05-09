d6d2bde73061a84a748a44886512599adfc0c92a
GIDDALURI-BALA-VENKATA-PRAVEEN <gbvp910@gmail.com>
2024-05-09 11:18:09 +0530

    this is for the first commit

---
+++ src/main/java/sonar/pro/com/Application1.java
@@ -0,0 +1,10 @@
+package sonar.pro.com;
+
+public class Application1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("This is for the sonar demo");
+	}
+
+}
