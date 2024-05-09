e07b488d19d329c3ff35c239e8fd8ed8f70eb56e
GIDDALURI-BALA-VENKATA-PRAVEEN <gbvp910@gmail.com>
2024-05-09 12:13:07 +0530

    over

--- src/main/java/sonar/pro/com/Application1.java
+++ src/main/java/sonar/pro/com/Application1.java
@@ -5,7 +5,11 @@ public class Application1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("This is for the sonar demo");
-		//this is for conflict demo text
+
+		
+
+      //this is demo for stash
+
 	}
 
 }
